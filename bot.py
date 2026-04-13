5593c1ab085d39dcd7c41d8e017c3762b3b11fc3
kolerowe-eng <kolerowe@gmail.com>
2026-04-13 14:02:08 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ def send_alert(message):
         print(f"Telegram Error: {e}")
 
 def get_kalshi_signal():
-    ticker = "KXINX-26APR13H1600-B6762"
+    ticker = "KXINX-26APR13H1600-B6837"
     url = f"https://api.elections.kalshi.com/trade-api/v2/markets/{ticker}"
     
     try:
